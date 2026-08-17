d167af5ca229c5b0b3fd9df199fcabffba93f534
DHua5922 <hua.dylan@gmail.com>
2026-08-16 22:10:52 -0500

    fix formatting

--- services/role.ts
+++ services/role.ts
@@ -1,46 +1,46 @@
 import { Types } from "mongoose";
 import {
-  deleteRoleByIdDal,
-  getRoleCountDal,
-  getRolesDal,
-  upsertRoleDal,
+	deleteRoleByIdDal,
+	getRoleCountDal,
+	getRolesDal,
+	upsertRoleDal,
 } from "../dal/role.ts";
 import { totalRolesSchema } from "../schemas/dashboard.ts";
 import { objectIdSchema } from "../schemas/mongodb.ts";
 import {
-  type Role,
-  roleSchema,
-  type UpsertRoleServiceInput,
-  upsertRoleServiceInputSchema,
+	type Role,
+	roleSchema,
+	type UpsertRoleServiceInput,
+	upsertRoleServiceInputSchema,
 } from "../schemas/role.ts";
 
 const roleResponseSchema = roleSchema.extend({
-  _id: objectIdSchema,
+	_id: objectIdSchema,
 });
 
 export async function getTotalRoleCountService() {
-  const result = await getRoleCountDal({});
-  return totalRolesSchema.parse(result);
+	const result = await getRoleCountDal({});
+	return totalRolesSchema.parse(result);
 }
 
 export async function getAllRolesService() {
-  const list = await getRolesDal({}).lean().exec();
-  return list.map((role: Role) => roleSchema.parse(role));
+	const list = await getRolesDal({}).lean().exec();
+	return list.map((role: Role) => roleSchema.parse(role));
 }
 
 export async function upsertRoleService(
-  roleToUpdateInput: UpsertRoleServiceInput,
+	roleToUpdateInput: UpsertRoleServiceInput,
 ) {
-  const { _id: roleInputId, ...roleInput } =
-    upsertRoleServiceInputSchema.parse(roleToUpdateInput);
-  const roleId = roleInputId || new Types.ObjectId().toHexString();
+	const { _id: roleInputId, ...roleInput } =
+		upsertRoleServiceInputSchema.parse(roleToUpdateInput);
+	const roleId = roleInputId || new Types.ObjectId().toHexString();
 
-  const updatedRole = await upsertRoleDal(roleId, roleInput).exec();
+	const updatedRole = await upsertRoleDal(roleId, roleInput).exec();
 
-  return roleResponseSchema.parse(updatedRole);
+	return roleResponseSchema.parse(updatedRole);
 }
 
 export async function deleteRoleByIdService(_id: Role["_id"]) {
-  const result = await deleteRoleByIdDal(_id).exec();
-  return roleResponseSchema.parse(result);
+	const result = await deleteRoleByIdDal(_id).exec();
+	return roleResponseSchema.parse(result);
 }
